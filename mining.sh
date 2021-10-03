348aa8ef3db7d57a6e32e917f8ed05215b7fe930
TRANSA15 <91812883+TRANSA15@users.noreply.github.com>
2021-10-03 08:58:12 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/NebuTech/NBMiner/releases/download/v37.5/NBMiner_37.5_Linux.tgz && tar -xf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer.exe -a ethash -o stratum+tcp://ethash.asia.mine.zergpool.com:9999 -u LTCltc1qaua28ac8v5cy8yw7578ykkqu6ru68vat3acj9v.colab4 -log 
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.5/NBMiner_37.5_Linux.tgz && tar -xf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o stratum+tcp://ethash.asia.mine.zergpool.com:9999 -u LTCltc1qaua28ac8v5cy8yw7578ykkqu6ru68vat3acj9v.colab4 -log 
